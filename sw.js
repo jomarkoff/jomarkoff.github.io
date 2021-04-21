c7b2994f21e8b0d50d06b81256e12ffa83308cf6
jomarkoff <jomarkoff@gmail.com>
2021-04-21 20:59:44 +0300

    upd

--- sw.js
+++ sw.js
@@ -2,8 +2,8 @@ var cacheName = 'hello-pwa';
 var filesToCache = [
   '/',
   '/index.html',
-  '/style.css',
-  '/sw.js'
+  '/css/style.css',
+  '/js/main.js'
 ];
 
 /* Start the service worker and cache all of the app's content */
@@ -24,38 +24,3 @@ self.addEventListener('fetch', function(e) {
     })
   );
 });
-
-
-
-// self.addEventListener('install', function(event) {
-//   var indexPage = new Request('index.html');
-//   event.waitUntil(
-//     fetch(indexPage).then(function(response) {
-//       return caches.open('pwabuilder-offline').then(function(cache) {
-//         console.log('[PWA Builder] Cached index page during Install'+ response.url);
-//         return cache.put(indexPage, response);
-//       });
-//   }));
-// });
-// self.addEventListener('fetch', function(event) {
-//   var updateCache = function(request){
-//     return caches.open('pwabuilder-offline').then(function (cache) {
-//       return fetch(request).then(function (response) {
-//         console.log('[PWA Builder] add page to offline '+response.url)
-//         return cache.put(request, response);
-//       });
-//     });
-//   };
-//   event.waitUntil(updateCache(event.request));
-//   event.respondWith(
-//     fetch(event.request).catch(function(error) {
-//       console.log( '[PWA Builder] Network request Failed. Serving content from cache: ' + error );
-//       return caches.open('pwabuilder-offline').then(function (cache) {
-//         return cache.match(event.request).then(function (matching) {
-//           var report =  !matching || matching.status == 404?Promise.reject('no-match'): matching;
-//           return report
-//         });
-//       });
-//     })
-//   );
-// })
